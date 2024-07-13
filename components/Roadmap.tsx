49cab0865f278c9ef0cda0335ae91d1d5c390e89
nauxscript <nauxscript@gmail.com>
2024-07-13 20:53:13 +0800

    fix: gap

--- components/Roadmap.tsx
+++ components/Roadmap.tsx
@@ -48,10 +48,10 @@ export default function RoadMap() {
   return (
     <section id="solutions" className="py-[100px] px-[80px]">
       <h2 className="text-5xl font-bold text-center">Roadmap</h2>
-      <div className="grid grid-cols-3 mt-[80px]">
+      <div className="grid grid-cols-3 mt-[80px] gap-y-48">
         {
           roadmapData.map(({title, events}, index) => (
-            <div key={index} className="relative pt-8 px-10 pb-48 border-t-2 border-orange-200">
+            <div key={index} className="relative pt-8 px-10 border-t-2 border-orange-200">
               <div className="w-4 h-4 bg-orange-400 absolute -top-2"></div>
               <h3 className="text-2xl text-bold text-slate-900">{title}</h3>
               <ul className="text-lg mt-5 text-slate-600 list-disc pl-4">
