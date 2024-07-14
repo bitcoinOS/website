d50c0ba0d8669861a5bc768856d5956b5fe5e8f2
lingxiyang <lansane@hotmail.com>
2024-07-14 13:24:40 +0800

    add dfx template

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Ubuntu } from "next/font/google";
+import {  Ubuntu } from "next/font/google";
 import "./globals.css";
 
 const ubuntu = Ubuntu({
