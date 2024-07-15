26d99b19dc58a0a0720cbc0aa64dc98309d68494
nauxscript <nauxscript@gmail.com>
2024-07-15 10:48:24 +0800

    feat: hero in mobile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,18 +2,16 @@ import Image from 'next/image';
 
 export default function Hero() {
   return (
-    <section className="relative w-full bg-gradient-to-br -mt-[60px] bg-[#fce4bc] from-[#fce4bc] via-pink-[#f3bda5] to-purple-300 text-white py-20 h-[700px] overflow-hidden">
-      <div className="flex h-full mx-24 text-left">
-        <div className="z-10 flex h-full flex-col justify-center w-[400px]">
-          <h1 className="text-[80px] leading-none font-bold italic">bitcoinOS</h1>
-          <p className="mt-8 text-2xl font-400">A Bitcoin Native Smart Contract & Finance Layer</p>
-          <button className="mt-8 btn bg-slate-950 btn-neutral btn-sm h-[44px] w-[160px] text-slate-200">Stake Now</button>
+    <section className="relative w-full bg-gradient-to-br -mt-[73px] md:-mt-[60px] bg-[#fce4bc] from-[#fce4bc] via-pink-[#f3bda5] to-purple-300 text-white pt-40 md:py-20 h-[700px] overflow-hidden">
+      <div className="flex h-full mx-6 md:mx-24 text-left">
+        <div className="z-10 relative flex h-full flex-col justify-center text-left w-[80%] md:w-[400px] text-slate-700">
+          <h1 className="text-[40px] md:text-[80px] leading-none font-bold italic">bitcoinOS</h1>
+          <p className="mt-5 text-lg md:mt-8 md:text-2xl font-400">A Bitcoin Native Smart Contract & Finance Layer</p>
+          <button className="hidden md:flex mt-8 btn bg-slate-950 btn-neutral btn-sm h-[44px] w-[160px] text-slate-200">Stake Now</button>
         </div>
-        <div className="w-2/3 z-0">
-          <Image src="/bitcoin_img_1.png" alt="" className='absolute -bottom-44 -right-16' width={880} height={880} />
-          <Image src="/bitcoin_img_2.png" alt="" className='absolute -bottom-14 left-[400px]' width={260} height={260} />
-          <Image src="/bitcoin_img_3.png" alt="" className='absolute top-20 right-[40%]' width={160} height={160} />
-        </div>
+          <Image src="/bitcoin_img_1.png" alt="" className='absolute -bottom-[23%] -right-[30%] md:-bottom-44 md:-right-16' width={880} height={880} />
+          <Image src="/bitcoin_img_2.png" alt="" className='absolute -left-[25%] top-28 md:top-auto md:-bottom-14 md:left-[400px]' width={260} height={260} />
+          <Image src="/bitcoin_img_3.png" alt="" className='absolute top-12 right-0 md:top-20 md:right-[40%]' width={160} height={160} />
       </div>
     </section>
   );
