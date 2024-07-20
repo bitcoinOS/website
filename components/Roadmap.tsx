ae64993ef11710aa91bc9a48878cfaa44f26135f
nauxscript <nauxscript@gmail.com>
2024-07-20 12:13:28 +0800

    feat: roadmap in mobile

--- components/Roadmap.tsx
+++ components/Roadmap.tsx
@@ -46,15 +46,15 @@ export default function RoadMap() {
     },
   ]
   return (
-    <section id="solutions" className="py-[100px] px-[80px]">
-      <h2 className="text-5xl font-bold text-center">Roadmap</h2>
-      <div className="grid grid-cols-3 mt-[80px] gap-y-48">
+    <section id="solutions" className="py-10 xl:py-[100px] px-5 xl:px-[80px] ">
+      <h2 className="text-3xl xl:text-5xl font-bold text-center">Roadmap</h2>
+      <div className="grid grid-cols-3 mt-6 xl:mt-[80px] xl:gap-y-48">
         {
           roadmapData.map(({title, events}, index) => (
-            <div key={index} className="relative pt-8 px-10 border-t-2 border-orange-200">
-              <div className="w-4 h-4 bg-orange-400 absolute -top-2"></div>
-              <h3 className="text-2xl text-bold text-slate-900">{title}</h3>
-              <ul className="text-lg mt-5 text-slate-600 list-disc pl-4">
+            <div key={index} className="col-span-5 xl:col-span-1 relative pt-8 px-10 border-l-2 xl:border-l-0 xl:border-t-2 border-orange-200">
+              <div className="w-4 h-4 bg-orange-400 absolute -left-2 top-10 xl:left-auto xl:-top-2"></div>
+              <h3 className="text-2xl font-bold text-slate-900">{title}</h3>
+              <ul className="text-sm leading-8 xl:text-lg mt-5 text-slate-600 list-disc pl-4">
                 {
                   events.map((event, index) => (
                     <li key={index}>{event}</li>
